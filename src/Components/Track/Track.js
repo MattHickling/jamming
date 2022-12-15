0cef6c8c6bb5abaf83c37968a4eb65d96188c1ce
Matt <m.hickling@hotmail.co.uk>
2022-12-15 16:39:16 +0000

    Spotify added

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,17 +3,31 @@ import React from "react";
 import './Track.css';
 
 class Track extends React.Component {
+    constructor(props){
+        super(props);
 
-    renderAction() {
-        if (this.props.isRemoval) {
-            return <button className="Track-action">-</button>
-        } else {
-            return <button className="Track-action">+</button>
-        }
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
 
     }
 
-    renders() {
+    renderAction() {
+        if (this.props.isRemoval) {
+            return <button className="Track-action" onClick={this.removeTrack}>-</button>
+        } else {
+            return <button className="Track-action" onClick={this.addTrack}>+</button>
+        }
+        
+    }
+    addTrack(){
+        this.props.onAdd(this.props.track);
+    }
+
+    removeTrack(){
+        this.props.onRemove(this.props.track)
+    }
+
+    render() {
         return (
             <div className="Track">
                 <div className="Track-information">
